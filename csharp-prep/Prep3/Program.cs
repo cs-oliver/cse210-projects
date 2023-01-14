66f913a24483571a5f6f285e9bc25e64ad8aaa9c
Cameron <cameron.s.oliver@gmail.com>
2023-01-14 16:39:04 -0700

    Completed Prep3 Activity

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -4,6 +4,28 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep3 World!");
+        Random randomGenerator = new Random();
+        int number = randomGenerator.Next(1, 101);
+
+        Console.Write("What is your guess?");
+        string guessinput = Console.ReadLine();
+        int guess = int.Parse(guessinput);
+
+        string response = "";
+
+        if (guess == number)
+        {
+            response = "Correct!";
+        }
+        else if (guess < number)
+        {
+            response = "Higher";
+        }
+        else if (guess > number)
+        {
+            response = "Lower";
+        }
+
+        Console.WriteLine($"{response}");
     }
 }
