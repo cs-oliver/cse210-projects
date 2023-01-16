1341ce52c01a42d780576d6b958919e80e2b9971
Cameron <cameron.s.oliver@gmail.com>
2023-01-15 18:27:36 -0700

    Completed Prep5, unknown error line 41

--- csharp-prep/Prep5/Program.cs
+++ csharp-prep/Prep5/Program.cs
@@ -4,6 +4,40 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep5 World!");
+        Welcome();
+        string userName = InputUserName();
+        int userNumber = InputUserNumber();
+        int squaredNumber = SquaredNumber(userNumber);
+        Display(userName, squaredNumber);
+    }
+
+    static void Welcome()
+    {
+        Console.WriteLine("Welcome to the Program!");
+    }
+
+    static string InputUserName()
+    {
+        Console.Write("Please enter your name: ");
+        string name = Console.ReadLine();
+        return name;
+    }
+
+    static int InputUserNumber()
+    {
+        Console.Write("Please enter your favorite number: ");
+        int number = int.Parse(Console.ReadLine());
+        return number;
+    }
+
+    static int SquaredNumber(int number)
+    {
+        int square = number * number;
+        return square;
+    }
+
+    static void Display(string name, int number)
+    {
+        Console.WriteLine($"{name}, the square of your number is {square}.");
     }
 }
